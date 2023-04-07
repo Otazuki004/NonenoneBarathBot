0ec61960ebb4f95bcca1355b41385fb83ee30bf9
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-07 10:13:27 +0530

    Create __init__.py

---
+++ MyTgBot/__init__.py
@@ -0,0 +1,21 @@
+
+import logging
+
+# enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('logs.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO)
+
+
+import os
+
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("TOKEN")
+
+
+from pyrogram import Client
+
+app = Client("MyTgBot", api_id, api_hash, bot_token)
