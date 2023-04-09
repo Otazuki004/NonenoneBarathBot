d71ecebc8d42961ae8b4f74b9fe9469ab9b0e6d7
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-09 20:59:25 +0530

    Update main.py

--- MyTgBot/plugins/main.py
+++ MyTgBot/plugins/main.py
@@ -5,12 +5,12 @@ from MyTgBot import app
 
 @app.on_message(filters.command("start"))
 async def start(_, message):
-     await message.reply_text("Your Start message Text")
+     await message.reply_text("System is alive!")
 
 
 # you can make this like more commands
 @app.on_message(filters.command("help"))
 async def help(_, message):
-     await message.reply_text("Your help Text")
+     await message.reply_text("here is a help")
 
 
