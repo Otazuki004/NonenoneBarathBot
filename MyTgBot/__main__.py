0a893b9fb36553496671bcaa8ba460864047f2af
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-10 07:45:15 +0530

    Update __main__.py

--- MyTgBot/__main__.py
+++ MyTgBot/__main__.py
@@ -1,9 +1,9 @@
-from MyTgBot import app, GROUP_ID
+from MyTgBot import bot, GROUP_ID
 
 
 if __name__ == "__main__":
-    app.run()
-    with app:
-       app.send_message(
+    bot.run()
+    with bot:
+       bot.send_message(
             chat_id=GROUP_ID, 
-            text="Am I Restarted!")
+            text="Bot started!")
