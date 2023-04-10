5fa7f3c0c689342f5b5813df4179ba93676ec341
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-10 09:49:37 +0530

    Update games.py

--- MyTgBot/plugins/games.py
+++ MyTgBot/plugins/games.py
@@ -4,27 +4,11 @@ from pyrogram.types import *
 import requests
 
 
-
+#Dice
 
 @bot.on_message(filters.command("dice"))
 async def roll_dice(bot, message):
     await bot.send_dice(message.chat.id, "🎲")
-
-@bot.on_message(filters.command(["dart","arrow"]))                                     
-async def roll_arrow(bot, message):
-    await bot.sent_dice(message.chat.id, "🎯")
-
-@bot.on_message(filters.command(["football","goal"]))
-async def roll_goal(bot, message):
-    await bot.sent_dice(message.chat.id, "⚽️")
-
-@bot.on_message(filters.command("roll"))
-async def roll_luck(bot, message):
-    await bot.sent_dice(message.chat.id, "🎰")
-
-@bot.on_message(filters.command(["throw","basket"]))
-async def roll_throw(bot, message):
-    await bot.sent_dice(message.chat.id, "🏀")
 
 
 
