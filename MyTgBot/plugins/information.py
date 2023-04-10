1907b2ac228225106d315d59f82eda23878d9cfc
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-10 08:23:54 +0530

    Update information.py

--- MyTgBot/plugins/information.py
+++ MyTgBot/plugins/information.py
@@ -91,3 +91,58 @@ id,name, username, mention,status, dc_id, bio),reply_to_message_id=message.id)
 id,name, username, mention,status, dc_id, bio),reply_to_message_id=message.id)
           except Exception as e:
               await message.reply_text(str(e))
+
+
+@bot.on_message(filters.command("id"))
+async def ids(_, message):
+      reply = message.reply_to_message
+      if reply:
+         id = ""
+         id += f"**Chat ID**: `{message.chat.id}`\n"
+         id += f"**Replied UID**: `{reply.from_user.id}`\n"
+         id += f"**Your UID**: `{message.from_user.id}`\n"
+         id += f"**Your Message ID**: `{message.id}`\n"
+         id += f"**Replied Message ID**: `{reply.id}`\n"
+         if reply.forward_from:
+             id += f"**Forward From ID**:\n`{reply.forward_from.id}`\n"
+         elif reply.left_chat_member:
+             id += f"**left Member ID**: `{reply.left_chat_member.id}`\n"
+         elif reply.new_chat_members:
+             for new_member in reply.new_chat_members:
+                   id += f"**New Member ID**: `{new_member.id}`\n"
+         elif reply.photo:
+             id += f"**Sent Photo ID**:\n`{reply.photo.file_id}`"
+         elif reply.animation:
+             id += f"**Sent Animation ID**:\n`{reply.animation.file_id}`"
+         elif reply.audio:
+             id += f"**Sent Audio ID**:\n`{reply.audio.file_id}`"
+         elif reply.document:
+             id += f"**Sent Animation ID**:\n`{reply.document.file_id}`"
+         elif reply.video:
+             id += f"**Sent Audio ID**:\n`{reply.video.file_id}`"
+         elif reply.sticker:
+             id += f"**Sent Sticker ID**:\n`{reply.sticker.file_id}`"
+         elif reply.voice:
+             id += f"**Sent Voice ID**:\n`{reply.voice.file_id}`"
+             
+         await message.reply(text=(id),disable_web_page_preview=True)
+      elif not reply:
+              if len(message.text.split()) <2:
+                  id = ""
+                  id += f"**Chat ID**: `{message.chat.id}`\n"
+                  id += f"**Your UID**: `{message.from_user.id}`\n"
+                  return await message.reply(text=(id),disable_web_page_preview=True)
+              elif len(message.text.split()) >2:
+                  return await message.reply("`wrong input!`")
+              username = message.text.split()[1]
+              id = ""
+              try:
+                 they = await Nandha.get_chat(username)
+                 id += f"**They UID**: `{they.id}`\n"
+                 id += f"**Chat ID**: `{message.chat.id}`\n"
+                 id += f"**Your UID**: `{message.from_user.id}`\n"
+                 await message.reply(text=(id), disable_web_page_preview=True)
+              except PeerIdInvalid:
+                    await message.reply("`forward user msg and reply or direct reply user to get id I can't find the user so!`")
+              except UsernameInvalid:
+                   await message.reply("`please check the given input. no user found!`")
