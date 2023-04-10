a9b046d056ff77ed7280359701321de496c83b13
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-10 12:21:07 +0530

    Update admins.py

--- MyTgBot/plugins/admins.py
+++ MyTgBot/plugins/admins.py
@@ -30,11 +30,6 @@ async def demotes(_, message):
 **Demoted User:** **{user.mention}**""")
    except Exception as errors:
            await message.reply(f"**Error**: {errors}")
-
-@bot.on_message(filters.command("ban"))
-def ban(bot, message):
-   bot.ban_chat_member(chat_id, user_id)
-   bot.send_message(chat.id, f"{message.reply_to_message.from_user.mention} Banned!")
        
 @bot.on_message(filters.command("unban"))
 async def unbanning(_, message):
