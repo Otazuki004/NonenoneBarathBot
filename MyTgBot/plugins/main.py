12d610c47da5df494c559efaf935f70a504e049e
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-10 12:43:52 +0530

    Update main.py

--- MyTgBot/plugins/main.py
+++ MyTgBot/plugins/main.py
@@ -1,16 +1,20 @@
 from pyrogram import filters
 from MyTgBot import bot
 
+START_TEXT = """
+I Already Awake!  ( • ̀ω•́  )!
 
+• Read the help menu for about my futures /help.
+"""
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
-     await message.reply_text("System is alive!")
+     await message.reply_text(START_TEXT)
 
 
 # you can make this like more commands
 @bot.on_message(filters.command("help"))
 async def help(_, message):
-     await message.reply_text("here is a help")
+     await message.reply_text("Click button below to know my commands")
 
 
