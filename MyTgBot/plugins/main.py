d4d78370fbc6cf305da5b3b0ea9c4cc808e596f3
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-10 07:46:52 +0530

    Update main.py

--- MyTgBot/plugins/main.py
+++ MyTgBot/plugins/main.py
@@ -1,15 +1,15 @@
 from pyrogram import filters
-from MyTgBot import app
+from MyTgBot import bot
 
 
 
-@app.on_message(filters.command("start"))
+@bot.on_message(filters.command("start"))
 async def start(_, message):
      await message.reply_text("System is alive!")
 
 
 # you can make this like more commands
-@app.on_message(filters.command("help"))
+@bot.on_message(filters.command("help"))
 async def help(_, message):
      await message.reply_text("here is a help")
 
