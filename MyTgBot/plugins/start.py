ff364ac924b2f5af6a14ace1206d159e4adb1c76
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-04-13 10:30:49 +0530

    Update start.py

--- MyTgBot/plugins/start.py
+++ MyTgBot/plugins/start.py
@@ -22,7 +22,7 @@ buttons = [
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
-     await message.reply(START_TEXT),
+     await message.reply(START_TEXT,
      reply_markup=InlineKeyboardMarkup(buttons),)
 
 @bot.on_message(filters.command("help"))
