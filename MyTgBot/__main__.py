57d3a1064cc29113c4cbccdaaa6e80151d45043a
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-13 19:21:14 +0530

    Update __main__.py

--- MyTgBot/__main__.py
+++ MyTgBot/__main__.py
@@ -4,5 +4,4 @@ import config
 if __name__ == "__main__":
     bot.run()
     with bot:
-       bot.send_message(
-           config.GROUP_ID, "Bot started!")
+       bot.send_sticker(config.GROUP_ID, sticker="CAACAgQAAx0CatX7ugAC1Q5je29sMbWkm8pX9KFL-LgW5MScSgACXgEAAiIN2QABm8LQQ_qSuqceBA")
