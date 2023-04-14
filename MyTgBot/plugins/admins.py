1d7ee829488fd45f0a12ffb5361d48f1c1a69113
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-14 11:56:18 +0530

    Update admins.py

--- MyTgBot/plugins/admins.py
+++ MyTgBot/plugins/admins.py
@@ -3,19 +3,6 @@ from MyTgBot import bot
 from pyrogram.types import *
 import os, io, time
 
-@bot.on_message(filters.command("ban"))
-async def ban(_, message):
-     if not bot_stats.privileges:
-         return await message.reply("**Lol! Make Me Admin When!**")
-     elif not user_stats.privileges:
-         return await message.reply("**You Needs Admin Rights to Control Me (~_^)!**")
-     elif not bot_stats.privileges.can_restrict_members:
-         return await message.reply("**I'm missing the admin rights `can_restrict_members**")
-     elif not user_stats.privileges.can_restrict_members:
-         return await message.reply("**You're missing the admin rights `can_restrict_members**")
-         reply = message.reply_to_message
-             bot.ban_chat_member(message.chat_id, reply.user_id)
-             bot.send_message(message.chat.id ,f"Banned! {reply.from_user.mention}")
 
 @bot.on_message(filters.command("demote"))
 async def demotes(_, message):
