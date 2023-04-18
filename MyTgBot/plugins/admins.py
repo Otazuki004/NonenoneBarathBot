af83bac43bf13c4acc66f945310848e67c04af10
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-18 20:34:22 +0530

    Update admins.py

--- MyTgBot/plugins/admins.py
+++ MyTgBot/plugins/admins.py
@@ -20,8 +20,8 @@ async def ban(_, message):
                 await message.reply_text(text= "**Ban Successfully**")
             else:
                 await message.reply_text(text = "**Your missing the admin rights `can_restrict_members`**")
-        except Exception as e:
-           await message.reply_text(e)
+        except Exception as errors:
+           await message.reply(f"**Error**: {errors}")
 
 
 @bot.on_message(filters.command("unban"))
@@ -40,8 +40,8 @@ async def unban(_, message):
                 await message.reply_text(text= "**Unban Successfully**")
             else:
                 await message.reply_text(text = "**Your missing the admin rights `can_restrict_members`**")
-        except Exception as e:
-           await message.reply_text(e)
+        except Exception as errors:
+           await message.reply(f"**Error**: {errors}")
 
 
 @bot.on_message(filters.command("demote"))
