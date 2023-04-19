4ae7625a4a80b726eb4f33bbbfb85527cdf05bb3
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-04-19 19:31:51 +0530

    Update __init__.py

--- MyTgBot/__init__.py
+++ MyTgBot/__init__.py
@@ -14,15 +14,13 @@ import os
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("TOKEN")
+group_id = os.getenv("GROUP_ID")
 
 
 from pyrogram import Client
 
 bot = Client("MyTgBot", 
-       api_id=api_id, 
-       api_hash=api_hash,
-       bot_token=bot_token,
+       api_id=API_ID, 
+       api_hash=API_HASH,
+       bot_token=TOKEN,
        plugins=dict(root="MyTgBot"), )
-
-
-GROUP_ID = os.getenv("GROUP_ID")
