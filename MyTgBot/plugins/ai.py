95b21db8ac68fbc5b6cb1684b364aa17b8900738
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-05-01 17:14:43 +0530

    Update ai.py

--- MyTgBot/plugins/ai.py
+++ MyTgBot/plugins/ai.py
@@ -9,12 +9,7 @@ BOT_ID = (6005606875)
 async def chatbot(_, message):
          if message.reply_to_message:
              await message.reply("Successfully ChatBot Active!")
-         else:
-             await message.reply("This Chat Already Enabled ChatBot!")
-        
-@bot.on_message(filters.text, group=200)
-async def addchat(_, message):
-          if not message.reply_to_message:
+         if not message.reply_to_message:
                 return
           elif not message.reply_to_message.from_user.id == BOT_ID:
                 return
