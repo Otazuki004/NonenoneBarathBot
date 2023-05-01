ca7b3f1d0978bafe4e80e9520d40c946b5bdc5c9
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-05-01 19:20:43 +0530

    Update ai.py

--- MyTgBot/plugins/ai.py
+++ MyTgBot/plugins/ai.py
@@ -7,9 +7,9 @@ BOT_ID = (6005606875)
 
 @bot.on_message(filters.text, group=200)
 async def chatbot(_, message):
-          elif not message.reply_to_message.from_user.id == BOT_ID:
+          if not message.reply_to_message.from_user.id == BOT_ID:
                 return
-          elif message.reply_to_message.from_user.id == BOT_ID:
+          if message.reply_to_message.from_user.id == BOT_ID:
                Message = message.text
                API = requests.get("https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/Serena/Anonymous/message="+Message).json()
                await message.reply(API["reply"])
