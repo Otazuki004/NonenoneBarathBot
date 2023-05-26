edd88a3f6ab86abefdf45f3a982faf1da6e7e7e8
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-05-26 08:04:09 +0530

    Update admins.py

--- MyTgBot/plugins/admins.py
+++ MyTgBot/plugins/admins.py
@@ -22,7 +22,7 @@ async def ban(_, message):
                 chat_id = message.chat.id
                 user_id  = message.reply_to_message.from_user.id
                 await bot.ban_chat_member(chat_id, user_id)
-                await message.reply_text(text= f"**Banned {message.from_user.mention}!**")
+                await message.reply_text(text= "**Banned {message.from_user.mention}!**")
             else:
                 await message.reply_text(text = "**Your missing the admin rights `can_restrict_members`**")
         except Exception as errors:
@@ -47,7 +47,7 @@ async def unban(_, message):
                 chat_id = message.chat.id
                 user_id  = message.reply_to_message.from_user.id
                 await bot.unban_chat_member(chat_id, user_id)
-                await message.reply_text(text= f"**Unbanned {}!**".format("[{user_id}](tg://user?id={user_id})"))
+                await message.reply_text(text= "**Unbanned {}!**".format(f"[{user_id}](tg://user?id={user_id})"))
             else:
                 await message.reply_text(text = "**Your missing the admin rights `can_restrict_members`**")
         except Exception as errors:
