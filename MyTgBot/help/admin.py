65d1ea8e8ab23bee79e12e6d97e760edc85acb06
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-06-06 15:37:00 +0530

    Update admin.py

--- MyTgBot/help/admin.py
+++ MyTgBot/help/admin.py
@@ -2,13 +2,6 @@ from MyTgBot import bot
 from pyrogram import enums
 
 
-async def is_owner(chat_id: int, user_id: int):
-    async for x in bot.get_chat_members(chat_id):
-        if x.status == enums.ChatMemberStatus.OWNER:
-             if x.user.id == user_id:
-                 return True
-             else: return False
-
 async def is_admin(chat_id: int, user_id: int):
      admin = await bot.get_chat_member(chat_id, user_id)
      if admin.privileges:
