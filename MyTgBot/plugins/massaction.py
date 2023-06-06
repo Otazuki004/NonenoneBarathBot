c9c453f63e75355e281470baf990130a49d3d90b
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-06-06 11:28:29 +0530

    Update massaction.py

--- MyTgBot/plugins/massaction.py
+++ MyTgBot/plugins/massaction.py
@@ -31,54 +31,59 @@ async def unbanall(_, message):
 async def banall(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    if not user_id in config.DEVS and (await is_owner(chat_id,user_id)) == False:
-           return await message.reply("`You Can't Access This!`")
+    if not get.privileges:
+          return await message.reply("`You Can't Access This!`")
+    if not get.privileges.can_restrict_members:
+          return await message.reply("`Your missing the admin rights `can_restrict_members``")
     elif message.chat.type == enums.ChatType.PRIVATE:
           return await message.reply("`This Command Only work in Groups!`")
     else:  
        try: 
           Members = []
           Admins = []
-          async for x in Nandha.get_chat_members(chat_id):
+          async for x in bot.get_chat_members(chat_id):
               if not x.privileges:
                     Members.append(x.user.id)
               else:
                     Admins.append(x.user.id)
           for user_id in Members:
                if message.text.split()[0].lower().startswith("s"):
-                        m = await Nandha.ban_chat_member(chat_id, user_id)
+                        m = await bot.ban_chat_member(chat_id, user_id)
                         await m.delete()
                else:
-                   await Nandha.ban_chat_member(chat_id, user_id)
+                   await bot.ban_chat_member(chat_id, user_id)
           await message.reply_text("**Successfully Banned**: `{}`\n**Remaining Admins**: `{}`".format(len(Members),len(Admins),))
        except Exception as e:
         print(e)
+     
 
-@Nandha.on_message(filters.command(["skickall","kickall","masskick"],config.CMDS))
+@bot.on_message(filters.command(["skickall","kickall","masskick"]))
 async def kickall(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-    if not user_id in config.DEVS and (await is_owner(chat_id,user_id)) == False:
-           return await message.reply("`You Can't Access This!`")
+    if not get.privileges:
+          return await message.reply("`You Can't Access This!`")
+    if not get.privileges.can_restrict_members:
+          return await message.reply("`Your missing the admin rights `can_restrict_members``")
     elif message.chat.type == enums.ChatType.PRIVATE:
           return await message.reply("`This Command Only work in Groups!`")
     else:  
        try: 
           Members = []
           Admins = []
-          async for x in Nandha.get_chat_members(chat_id):
+          async for x in bot.get_chat_members(chat_id):
               if not x.privileges:
                     Members.append(x.user.id)
               else:
                     Admins.append(x.user.id)
           for user_id in Members:
                if message.text.split()[0].lower().startswith("s"):
-                        m = await Nandha.ban_chat_member(chat_id, user_id)
-                        await Nandha.unban_chat_member(chat_id, user_id)
+                        m = await bot.ban_chat_member(chat_id, user_id)
+                        await bot.unban_chat_member(chat_id, user_id)
                         await m.delete()
                else:
-                   await Nandha.ban_chat_member(chat_id, user_id)
-                   await Nandha.unban_chat_member(chat_id, user_id)
+                   await bot.ban_chat_member(chat_id, user_id)
+                   await bot.unban_chat_member(chat_id, user_id)
           await message.reply_text("**Successfully Kicked**: `{}`\n**Remaining Admins**: `{}`".format(len(Members),len(Admins),))
        except Exception as e:
         print(e)
