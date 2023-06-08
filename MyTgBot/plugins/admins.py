341b76f10a0d54c122c0109c06bfcc53f4ba8f91
BARATH <115382856+ITZ-BARATH@users.noreply.github.com>
2023-06-08 08:42:28 +0530

    Update admins.py

--- MyTgBot/plugins/admins.py
+++ MyTgBot/plugins/admins.py
@@ -61,7 +61,7 @@ async def unban(_, message):
                 chat_id = message.chat.id
                 user_id  = message.reply_to_message.from_user.id
                 await bot.unban_chat_member(chat_id, user_id)
-                await message.reply_text(text= "**Unban Successfully**")
+                await message.reply_text(text= "**Fine, they can join again.**")
             else:
                 await message.reply_text(text = "**Your missing the admin rights `can_restrict_members`**")
         except Exception as errors:
