7773b55a2a60b94d8c68029c117ecbd86263fc59
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-08-27 08:22:25 +0530

    Update __init__.py

--- MyTgBot/__init__.py
+++ MyTgBot/__init__.py
@@ -1,11 +1,10 @@
+from pyrogram import Client
 import os
 
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("TOKEN")
 
-
-from pyrogram import Client
 
 bot = Client("MyTgBot", 
        api_id=api_id, 
