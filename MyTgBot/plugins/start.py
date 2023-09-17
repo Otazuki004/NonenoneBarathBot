e749d32cf3a2590ce3962a0a3663e1cb0ab3f649
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-09-17 08:55:59 +0530

    Update start.py

--- MyTgBot/plugins/start.py
+++ MyTgBot/plugins/start.py
@@ -22,8 +22,12 @@ buttons = [
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
-     await message.reply_text(START_TEXT,
-     reply_markup=InlineKeyboardMarkup(buttons),)
+    if message.chat.type == "private":
+       await message.reply_text(START_TEXT,
+       reply_markup=InlineKeyboardMarkup(buttons),)
+    else:
+       pm_msg = f"Hello {mention}! PM me if you have any questions on how to use me!"
+       await message.reply_text(pm_msg)
 
 @bot.on_message(filters.command("help"))
 async def help(_, message):
