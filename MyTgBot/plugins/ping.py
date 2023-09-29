09fcbdbe26b43b45bbce39f0469686fffe2788c1
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-09-29 19:18:48 +0530

    Update ping.py

--- MyTgBot/plugins/ping.py
+++ MyTgBot/plugins/ping.py
@@ -9,7 +9,7 @@ from MyTgBot.help.readable_time import get_readable_time
 async def ping(_, message):
     currentTime = get_readable_time(time.time() - start)
     start_t = time.time()
-    rm = await message.reply_photo("https://graph.org//file/6ede45ee5dd3ecfc7314e.jpg", caption="Pong..")
+    rm = await message.reply_photo("https://telegra.ph/file/3f7514ba939b5df5862da.jpg", caption="Pong..")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
     await rm.edit_caption(
