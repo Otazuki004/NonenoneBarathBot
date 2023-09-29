1be2091b05e6a5d09e8c6cb4256a9785139e1ef5
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-09-29 19:02:45 +0530

    Create ping.py

---
+++ MyTgBot/plugins/ping.py
@@ -0,0 +1,21 @@
+import time
+from pyrogram import filters
+
+from MyTgBot import bot, start, version
+from MyTgBot.help.readable_time import get_readable_time
+
+
+@bot.on_message(filters.command("ping"))
+async def ping(_, message):
+    currentTime = get_readable_time(time.time() - start)
+    start_t = time.time()
+    rm = await message.reply_photo("https://graph.org//file/6ede45ee5dd3ecfc7314e.jpg", caption="Pong..")
+    end_t = time.time()
+    time_taken_s = round(end_t - start_t, 3)
+    await rm.edit_caption(
+        f"""
+**BOT VERSION:** {version}
+
+**PING:** {time_taken_s} seconds
+**UPTIME:** {currentTime}"""
+    )
