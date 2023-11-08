a6a820d52e13fbb5866ee0e0b7e76a65714897c3
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-11-08 14:24:09 +0530

    Create lock.py

---
+++ MyTgBot/plugins/lock.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+from pyrogram.types import ChatPermissions
+from MyTgBot import bot
+
+@bot.on_message(filters.command("lock"))
+async def lock(_, m):
+    await bot.set_chat_permissions(m.chat.id, ChatPermissions(can_send_messages=False))
+    await reply_and_delete(m, LOCKED)
+
+LOCKED = "Locked successfully!"
+UNLOCKED = "Unlocked successfully!"
+
+@bot.on_message(filters.command("unlock"))
+async def unlock(_, m):
+    await bot.set_chat_permissions(m.chat.id, PERMISSIONS[m.chat.id])
+    await reply_and_delete(m, UNLOCKED)
+
+PERMISSIONS = {
+    ChatPermissions(
+        can_send_messages=True,
+        can_send_media_messages=True,
+        can_send_stickers=True,
+        can_send_animations=True,
+        can_send_games=True,
+        can_use_inline_bots=True,
+        can_send_polls=True
+    )
+}
