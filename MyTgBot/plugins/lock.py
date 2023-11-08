0e97ea28d7f7c1a2a62d878c44481299eb962009
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-11-08 17:19:51 +0530

    Update lock.py

--- MyTgBot/plugins/lock.py
+++ MyTgBot/plugins/lock.py
@@ -5,28 +5,24 @@ from MyTgBot import bot
 
 @bot.on_message(filters.command("lock"))
 async def lock(_, m):
-if not get.privileges:
+   if not get.privileges:
     return await message.reply("-› Sorry, **only admins** can execute this command!")
     await bot.set_chat_permissions(m.chat.id, ChatPermissions(can_send_messages=False))
     lock = await m.reply_text(LOCKED)
     await sleep(10)
     await lock.delete()
-except Exception as errors:
-      await message.reply(f"**Error**: {errors}")
 
 LOCKED = "Locked successfully!"
 UNLOCKED = "Unlocked successfully!"
 
 @bot.on_message(filters.command("unlock"))
 async def unlock(_, m):
-if not get.privileges:
+   if not get.privileges:
     return await message.reply("-› Sorry, **only admins** can execute this command!")
     await bot.set_chat_permissions(m.chat.id, PERMISSIONS[m.chat.id])
     unlock = await m.reply_text(UNLOCKED)
     await sleep(10)
     await unlock.delete()
-except Exception as errors:
-      await message.reply(f"**Error**: {errors}")
 
 PERMISSIONS = ChatPermissions(
         can_send_messages=True,
