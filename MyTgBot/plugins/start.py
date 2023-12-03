aef3702630e532fe932b59b115b7b2620f7a796a
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2023-12-03 08:04:36 +0530

    .!

--- MyTgBot/plugins/start.py
+++ MyTgBot/plugins/start.py
@@ -9,7 +9,7 @@ from MyTgBot import bot
 START_TEXT = """
 Hello there i am Serena ✘
 
-I have lot of useful features and i can easily manage your groups! /help.
+I have lot of useful features and i can easily manage your groups!
 """
 
 buttons = [
