bb94c1f0b4cccc98ca5414f888d2c34d27037390
BARATH <115382856+BARATH-XD@users.noreply.github.com>
2024-01-16 15:41:23 +0530

    Update notes.py

--- MyTgBot/plugins/notes.py
+++ MyTgBot/plugins/notes.py
@@ -22,6 +22,8 @@ async def notes(_, message):
 @bot.on_message(filters.command("clear"))
 async def clear(_, message):
       chat_id = message.chat.id
+      get = await bot.get_chat_member(message.chat.id, message.from_user.id)
+      elif not get.privileges: return await message.reply_text("Admins Only Can Clear Notes!")
       try: note_name = message.text.split(None,1)[1]
       except: return await message.reply_text("what I want do clear? tell me note name!")
       x = db.find_one({"chat_id": chat_id, "note_name": note_name})
